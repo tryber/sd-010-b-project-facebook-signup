e842edaf8f3ef1553b0ac31a4be54b29327294d6
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-03-23 12:23:17 -0300

    requisito 20

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const getTextInput = document.getElementsByClassName('inputForm');
 const formButton = document.getElementById('facebook-register');
 const textCamp = document.getElementById('divId');
 const optionalGender = document.createElement('input');
+
 function validateText() {
   let emptyInput = false;
   for (let x = 0; x < getTextInput.length; x += 1) {
@@ -16,13 +17,6 @@ function validateText() {
   return emptyInput;
 }
 
-formButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (validateText()) {
-    document.getElementById('erro').innerText = 'Campos inválidos';
-  }
-});
-
 function createCamp() {
   textCamp.appendChild(optionalGender);
   optionalGender.name = 'gender-custom';
@@ -31,4 +25,34 @@ function createCamp() {
 
 document.getElementById('personal').addEventListener('click', () => {
   createCamp();
+});
+
+function getGender() {
+  let gender = '';
+  for (let x = 0; x < document.getElementsByClassName('inputGender').length; x += 1) {
+    if (document.getElementsByClassName('inputGender')[x].checked) {
+    gender = (document.getElementsByClassName('inputGender')[x].value);  
+    }
+  }
+  return (gender);
+};
+
+function createPhrase() {
+  let phrase = '';
+  const firstname = document.getElementsByClassName('inputForm')[0];
+  const lastname = document.getElementsByClassName('inputForm')[1];
+  const email = document.getElementsByClassName('inputForm')[2];
+  const birhtdate = document.getElementsByClassName('inputForm')[4];
+  const gender = getGender();
+  phrase = `Olá, ${firstname} ${lastname} ${email} ${birhtdate} ${gender}`
+  return (phrase);
+};
+
+formButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (validateText()) {
+    document.getElementById('erro').innerText = 'Campos inválidos';
+  } else {
+    document.getElementsByClassName('right-content')[0].innerHTML = createPhrase();
+  }
 });
