cf6a8d05846584e55d7b618db94637d65e1793a2
bidaffonso <bidaffondo@gmail.com>
2021-03-24 19:04:55 -0400

    Teste opçao de soluçao requisito 18

--- script.js
+++ script.js
@@ -9,16 +9,27 @@ function alerta() {
   alert(user.value);
 }
 
-function verificarTexto() {
-  const campos = document.querySelectorAll('.teste');
-  campos.forEach((item) => {
-    if (item.value === '') {
+// function verificarTexto() {
+//   const campos = document.querySelectorAll('.teste');
+//   campos.forEach((item) => {
+//     if (item.value === '') {
+//       alertIvalidField.innerHTML = 'Campos inválidos';
+//       item.classList.add('error');
+//     }
+//   });
+// }
+
+function verifyText() {
+  const campos = document.querySelectorAll(':required');
+  for (let index = 0; index < campos.length; index =+ 1) {
+    if (campos[index].value === '') {
       alertIvalidField.innerHTML = 'Campos inválidos';
-      item.classList.add('error');
+      return false;
     }
-  });
+  }
 }
 
 //  Chamadas das funções:
 submitBtn.addEventListener('click', alerta);
-registerBtn.addEventListener('click', verificarTexto);
+// registerBtn.addEventListener('click', verificarTexto);
+registerBtn.addEventListener('click', verifyText);
