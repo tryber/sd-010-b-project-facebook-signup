89c6f54c9698bf4287f00634e448d1c2d88b1f02
Dieghonm <dieghonm@gmail.com>
2021-03-25 01:21:51 -0300

    Quesito 19, tentativa 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const inputEmailPhone = document.getElementById('user-email-phone');
 const buttonGetIn = document.getElementById('button-login');
-const buttonSubmit = document.getElementById('facebook-register');
+const genderPersonality = document.getElementById('input-gender-personality');
 
 function buttonIn() {
   alert(inputEmailPhone.value);
@@ -11,9 +11,9 @@ buttonGetIn.addEventListener('click', buttonIn);
 /* Requisito 18: verificando se algum dos campos é vazio: */
 function validateForm() {
   let isValid = true;
-  inputs = ['firstname', 'lastname', 'phone_email', 'password', 'birthdate', 'gender'];
+  let inputs = ['firstname', 'lastname', 'phone_email', 'password', 'birthdate', 'gender'];
   for (let input of inputs) {
-    content = document.forms["form"][input].value;
+    let content = document.forms["form"][input].value;
     if (content === '') {
       isValid = false;
     }
@@ -21,3 +21,12 @@ function validateForm() {
   if (!isValid) alert('Campos inválidos');
   return false;
 }
+
+const genderCampos = genderPersonality.addEventListener ('click', () => {
+  const divPai = document.getElementById('campo-gender-personality')
+  const input = document.createElement('input')
+  input.type = 'text'
+  input.name = 'gender-custom'
+  input.placeholder = 'Gênero (opcional)'
+  divPai.appendChild(input)
+})
