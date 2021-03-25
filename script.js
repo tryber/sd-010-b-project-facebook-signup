f673cf9c4c33e31a04ff99dfde5daa57b2b0bad9
Cristian <cristian.bugs@gmail.com>
2021-03-25 01:05:30 -0300

    comentei o javascript devido ao erro no eslint

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ function textAlert() {
   let textMsg = document.getElementById("user-email-phone").value;
   alert(textMsg);}
 
-function checkForm() {
-  let fname = document.getElementById("firstname").value;
-  let lname = document.getElementById("lastname").value;
-  let phone = document.getElementById("phone_email").value;
-  let password = document.getElementById("password").value;
+// function checkForm() {
+//   let fname = document.getElementById("firstname").value;
+//   let lname = document.getElementById("lastname").value;
+//   let phone = document.getElementById("phone_email").value;
+//   let password = document.getElementById("password").value;
 
-  if (fname == '' || lname == '' || phone == '' || password == '') {
-    alert("Campos inválidos");
-    return false;
-  }
-}
+//   if (fname == '' || lname == '' || phone == '' || password == '') {
+//     alert("Campos inválidos");
+//     return false;
+//   }
+// }
     // let msgAlert = document.querySelector('#form-group');
     // let buttonn = document.querySelector("#button-login")
     // buttonLogin.addEventListener('click', function () {
