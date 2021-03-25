d54f061a2f9cfdfb63e8e50b4b3089d95a5543eb
Cristian <cristian.bugs@gmail.com>
2021-03-25 10:10:15 -0300

    funcoes javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+let field = document.querySelector('#"button-login"');
+
+function emailOrPhone () {
+  let first = document.querySelector('#user-email-phone');
+  let second = document.querySelector('#"user-password"');
+  if( first == '' || second == '') {
+    alert("Campos inválidos")
+    }
+})
+
+function checkForm() {
+  let fname = document.getElementById("firstname").value;
+  let lname = document.getElementById("lastname").value;
+  let phone = document.getElementById("phone_email").value;
+  let password = document.getElementById("password").value;
+  ​
+  if (fname == '' || lname == '' || phone == '' || password == '') {
+    alert("Campos inválidos");
+    return false;
+  }
+}
