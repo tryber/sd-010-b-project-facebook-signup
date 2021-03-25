92cdbbf19298134786edce6f45441e685edeee56
walterlbf <walter.lbf.f@gmail.com>
2021-03-25 11:55:27 -0400

    indentacao linhas 11 13 23

--- script.js
+++ script.js
@@ -8,19 +8,18 @@ function exibeAlerta() {
 
 clickBotao.addEventListener('click', exibeAlerta);
 
-clickCad.addEventListener('click', campoInvalido);
-
 function campoInvalido() {
 
   const clickSiginup = document.querySelectorAll('input');
   let cont = 0;
-
   for (let index = 0; index < clickSiginup.length; index += 1) {
     if (clickSiginup[index].value === '') {
       cont += 1;
     }
   }
-  if (cont != 0) {
+  if (cont !== 0) {
     alert('Campos inválidos');
   }
 }
+
+clickCad.addEventListener('click', campoInvalido);
