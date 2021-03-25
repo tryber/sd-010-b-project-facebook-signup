3c42699e58b66e4d95e40351a8f55b223f64e167
Junior030 <juniorhenrique030@gmail.com>
2021-03-25 20:22:35 -0300

    Campo obrigatorio

--- script.js
+++ script.js
@@ -20,31 +20,61 @@ buttonLogin.addEventListener('click', () => {
   alert(document.querySelector('#user-email-phone').value);
 });
 
-const inputCadastro = document.querySelectorAll('.right-content input');
-const inputRadio = document.querySelectorAll('#gender-container input');
+const inputText = document.querySelectorAll('.validInput');
+const inputRadio = document.querySelectorAll('.validInputRadio');
 const cadastrar = document.querySelector('#facebook-register');
-const aux = [];
-let aux2 = false;
 
-function printInvalido() {
-  if ((aux.length < 4) || (aux2 === false)) {
+function validFormInput(inputText) {
+  let value = [];
+  for (let i = 0; i < inputText.length; i += 1) {
+    (inputText[i].value === '') ? value[i] = false : value[i] = true;
+  }
+  return value;
+}
+
+function validInputRadio(inputRadio) {
+  let value = -1;
+  for (let i = 0; i < inputRadio.length; i += 1) {
+    if (inputRadio[i].checked) {
+      value = i;
+    }
+  }
+  return value;
+}
+
+function paremt (valid1, valid2) {
+  let aux = [];
+  for (let i = 0; i < valid1.length; i += 1) {
+    if (valid1[i] === false) {
+      aux[0] = true;
+    }
+  }
+  if (aux[0] !== true) {
+    aux[0] = false;
+  }
+  (valid2 === -1) ? aux[1] = true : aux[1] = false;  
+  if (valid2 === 2) {
+    if (document.querySelector('.validInputRadioPersonalizado').value === '') {
+      aux[1] = false;
+    } 
+  }
+  return aux;
+}
+
+function printCampo(paremt) {
+  if ((paremt[0] === true) || (paremt[1] === true)) {
     const alerta = document.querySelector('footer');
-    const p = document.createElement('p');
+    const p = document.createElement('span');
     p.innerHTML = 'Campos inválidos';
     p.style.fontSize = '25px';
     p.style.fontWeight = '900';
     alerta.appendChild(p);
-  }
+  } 
 }
 
-cadastrar.addEventListener('click', () => {
-  for (let i = 0; i < inputCadastro.length - 5; i += 1) {
-    aux.push(inputCadastro[i].value);
-  }
-  for (let i = 0; i < inputRadio.length; i += 1) {
-    if (inputRadio[i].checked === true) {
-      aux2 = true;
-    }
-  }
-  printInvalido();
+cadastrar.addEventListener('click', () => {   
+  const valid1 = validFormInput(inputText);
+  const valid2 = validInputRadio(inputRadio);
+  const parametro = paremt(valid1, valid2);
+  printCampo(parametro);
 });
