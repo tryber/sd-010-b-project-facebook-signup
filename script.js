ef5802528104a122ace5104090374c8298ceca68
Jander Liborio <jandernunes14@gmail.com>
2021-03-25 18:38:00 -0400

    campos invalidos, lint js

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ buttomLogin.addEventListener('click', () => {
 });
 
 const radioValue = document.querySelector('input[name="gender"]:checked');
-const firstname = document.getElementById("firstname");
-const lastname = document.getElementById("lastname");
-const phone_email = document.getElementById("phone_email");
-const password = document.getElementById("password");
-const birthdate = document.getElementById("birthdate");
-const cadastre = document.getElementById("facebook-register");
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const phoneEmail = document.getElementById('phone_email');
+const password = document.getElementById('password');
+const birthdate = document.getElementById('birthdate');
+const cadas = document.getElementById('facebook-register');
 
-cadastre.addEventListener('click', () => {
-  if (radioValue.value === '' || firstname.value === '' || lastname.value === '' || phone_email.value === ''|| password.value === '' || birthdate.value === '' || cadastre.value === '') {
-    alert('Campos Invaálidos')
+cadas.addEventListener('click', () => {
+  if (radioValue.value === '' || firstname.value === '' || lastname.value === '' ||phoneEmail.value === '') {
+    alert('Campos Invaálidos');
+  }
+  if (password.value === '' || birthdate.value === '' || cadastre.value === '') {
+    alert('Campos Invaálidos');
   }
 });
