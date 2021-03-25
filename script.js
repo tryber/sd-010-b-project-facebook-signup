a1f93cba36a8bada60f51b82ebaf53d88ca01794
Cristian <cristian.bugs@gmail.com>
2021-03-25 00:52:01 -0300

    novas funcoes e nome e sobrenome lado a lado

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 function textAlert() {
-  alert(document.getElementById('user-email-phone').value);
+  let textMsg = document.getElementById("user-email-phone").value;
+  alert(textMsg);
 }
-textAlert();
+​
+​
+function checkForm() {
+  let fname = document.getElementById("firstname").value;
+  let lname = document.getElementById("lastname").value;
+  let phone = document.getElementById("phone_email").value;
+  let password = document.getElementById("password").value;
+  ​
+  if (fname == '' || lname == '' || phone == '' || password == '') {
+    alert("Campos inválidos");
+    return false;
+  }
+}
+    // let msgAlert = document.querySelector('#form-group');
+    // let buttonn = document.querySelector("#button-login")
+    // buttonLogin.addEventListener('click', function () {
+    // msgAlert.innerHTML = 'Campos inválidos';
+    // }
+
