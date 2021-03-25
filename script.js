541ab4c91d1ea5883be995bcce84e3561a661396
Jander Liborio <jandernunes14@gmail.com>
2021-03-25 18:32:03 -0400

    campos invalidos

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const loginValue = document.getElementById('user-email-phone');
 buttomLogin.addEventListener('click', () => {
   alert(loginValue.value);
 });
+
+const radioValue = document.querySelector('input[name="gender"]:checked');
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const phone_email = document.getElementById("phone_email");
+const password = document.getElementById("password");
+const birthdate = document.getElementById("birthdate");
+const cadastre = document.getElementById("facebook-register");
+
+cadastre.addEventListener('click', () => {
+  if (radioValue.value === '' || firstname.value === '' || lastname.value === '' || phone_email.value === ''|| password.value === '' || birthdate.value === '' || cadastre.value === '') {
+    alert('Campos Invaálidos')
+  }
+});
