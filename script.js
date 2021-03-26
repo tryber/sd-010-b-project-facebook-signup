8f10b291b574fff98b237196c61f469efea14d3a
root <root@DESKTOP-ROBTTQD.localdomain>
2021-03-26 19:07:35 -0300

    requisitos 18 e 19

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const login = document.getElementById('button-login');
 const message = document.getElementById('user-email-phone');
+const cadastreSe = document.getElementById('button-login');
 
 function clickLogin() {
   alert(message.value);
@@ -7,22 +8,52 @@ function clickLogin() {
 
 login.addEventListener('click', clickLogin);
 
+
 function validando(){
-  const nome = document.getElementById('firstname');
-  const sobreNome = document.getElementById('lastname');
-  const emailTelefone = document.getElementById('phone_email');
-  const senha = document.getElementById('password');
-  const aniversario = document.getElementById('birthdate');
   const cadastreSe = document.getElementById('facebook-register');
-  
-  cadastreSe.addEventListener('click', function () {
-    
-    if ( nome.value === '' || sobreNome.value === '' || emailTelefone.value === '' || senha.value === '' || aniversario.value === '' ) {
-      return alert('Campos inválidos');
+  const input = document.querySelectorAll(".inputs"); 
+  const pai = document.getElementById('formulario');
+  cadastreSe.addEventListener('click',function (event){
+  for (let index = 0; index < input.length; index += 1) {
+    if (input[index].value === '') {
+      const invalido = document.createElement('p');
+      invalido.innerText = 'Campos inválidos';
+      invalido.id = 'campoInvalido'
+      pai.appendChild(invalido);
+      break;
     }
-    return alert('enviado');
-    } 
-  )
+  }
+} 
+)
 }
-validando()
+validando();
+
+
+const generos = document.querySelector('#quadrogenero');
+function apagandoInput(novoGenero) {
+  const inputGeneroNovo= document.querySelector('#inputGeneroNovo');
+  if (inputGeneroNovo!== null) {
+    novoGenero.removeChild(inputGeneroNovo);
+  }
+}
+
+generos.addEventListener('click', (e) => {
+  if (e.target.type === 'radio') {
+    const novoGenero = document.querySelector('#inputPersonalizado');
+    const inputGeneroNovo= document.createElement('input');
+    console.log('passou no if 1')
+    if (e.target.id === 'personalizado') {
+      apagandoInput(novoGenero);
+      inputGeneroNovo.placeholder = 'Gênero (opcional)';
+      inputGeneroNovo.name = 'gender-custom';
+      novoGenero.appendChild(inputGeneroNovo);
+      inputGeneroNovo.id = 'inputGeneroNovo';
+      console.log('passou no if 2')
+    } else {
+      apagandoInput(novoGenero);
+      console.log('passou no else')
+    }
+  }
+});
+
 
