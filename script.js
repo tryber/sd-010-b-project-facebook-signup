c43dfc59326027239f099a26f615b1dd938d0a95
Valeriandreoni <valeriandreoni@gmail.com>
2021-03-25 22:20:32 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const login = document.getElementById('button-login');
-
 const message = document.getElementById('user-email-phone');
+const cadastreSe = document.getElementById('button-login');
 
 function clickLogin() {
   alert(message.value);
 }
 
 login.addEventListener('click', clickLogin);
+/* []validar os campos quando aperta cadastre
+[] colocar opção de texto quando clicado em personalizado
+[]  requisito ultimo */
+
+function validando(){
+  const nome = document.getElementById('firstname');
+  const sobreNome = document.getElementById('lastname');
+  const emailTelefone = document.getElementById('phone_email');
+  const senha = document.getElementById('password');
+  const aniversario = document.getElementById('birthdate');
+  const cadastreSe = document.getElementById('facebook-register');
+  
+  cadastreSe.addEventListener('click', function(){
+    
+    if ( nome.value===''|| sobreNome.value===''|| emailTelefone.value===''|| senha.value==='' || aniversario.value===''){
+      return alert('Campos inválidos');
+    }else {
+      return alert('enviado');
+    } 
+  })
+}
+validando()
