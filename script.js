a00b555e88ada22c387bae63c6f9631e8e79bca7
Jander Liborio <jandernunes14@gmail.com>
2021-03-26 18:30:17 -0400

    campos invalidos parte 3.4

--- script.js
+++ script.js
@@ -6,21 +6,24 @@ buttomLogin.addEventListener('click', () => {
 
 const cadastro = document.getElementById('facebook-register');
 // const radioValue = document.querySelector('input[name="gender"]:checked');
-const contaneirForms = document.querySelector('.oi');
 
 const verificaNull = () => {
   const invalidos = document.querySelectorAll('.camposInvalidos');
-  let valido = false;
+  let valido;
   for (let index = 0; index < invalidos.length; index += 1) {
     if (invalidos[index].value === '') {
       valido = true;
+    } else {
+      valido = false;
     }
   }
   return valido;
 };
 
-cadastro.addEventListener('click', () => {
-  const text = document.createElement('div');
+cadastro.addEventListener('click', (event) => {
+  event.preventDefault()
+  const contaneirForms = document.querySelector('.thingamagig');
+  const text = document.createElement('p');
   text.innerText = 'Campos inválidos';
   if (verificaNull()) {
     contaneirForms.appendChild(text);
