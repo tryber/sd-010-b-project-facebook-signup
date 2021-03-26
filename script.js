f5f4858df645c676506e5ecd136bf74f11f6b70f
Michelle <mls.srbek@gmail.com>
2021-03-26 20:00:21 -0300

    criando branch propria

--- script.js
+++ script.js
@@ -17,23 +17,10 @@ function customGender() {
   clicked = true;
 }
 
-// function removeCustomGender() {
-//   const fem = document.getElementById('feminino');
-//   const masc = document.getElementById('masculino');
-//   if ((fem.checked === true) || (masc.checked === true)) {
-//     console.log('okay');
-//     // const parent = document.getElementsByClassName('genders')[0];
-//   }
-// }
-
 window.onload = function () {
   const buttonLogin = document.getElementById('button-login');
   buttonLogin.addEventListener('click', emailTelephoneAlert);
 
   const genderCustom = document.getElementById('custom-gender');
   genderCustom.addEventListener('click', customGender);
-  // const feminino = document.getElementById('feminino');
-  // feminino.addEventListener('click', removeCustomGender);
-  // const masculino = document.getElementById('masculino');
-  // masculino.addEventListener('click', removeCustomGender);
 };
