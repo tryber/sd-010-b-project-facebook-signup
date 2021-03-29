686cdb753a6985fd8a7e2d4f98439d25d1c2469b
Mariana Mohr <marianamohr@gmail.com>
2021-03-29 09:57:03 -0300

    Requisito 10 - subustituir o form pelos dados da pessoa

--- script.js
+++ script.js
@@ -3,8 +3,45 @@ function alerta() {
   alert(email);
 }
 
-function para(event) {
+function paraEvento(event) {
   event.preventDefault();
+}
+
+function limpaForm() {
+  const pai = document.getElementById('formcontato');
+  const listaDivs = document.getElementsByClassName('removivel');
+
+
+  for (let i = 0; i < listaDivs.length; i += 1) {
+    if (listaDivs[i].classList.contains('removivel') === true) {
+      listaDivs[i].classList.add('remover');
+      // pai.removeChild(listaDivs[i]);
+    }
+  }
+}
+
+function imprimeDados() {
+  const array = document.getElementsByClassName('imprimir');
+  const els = document.getElementsByClassName('checked-genero');
+  let genero;
+  for (let i = 0; i < els.length; i++) {
+    if (els[i].checked) {
+      genero = (els[i].value);
+    }
+  }
+  const pai = document.getElementById('formcontato');
+  const pNome = document.createElement('p');
+  const pEmail = document.createElement('p');
+  const pData = document.createElement('p');
+  const pGenero = document.createElement('p');
+  pNome.innerHTML = `Olá, ${array[0].value} ${array[1].value}`;
+  pai.appendChild(pNome);
+  pEmail.innerHTML = `Email ou Telefone: ${array[2].value}.`;
+  pai.appendChild(pEmail);
+  pData.innerHTML = `Data de Nascimento: ${array[3].value}.`;
+  pai.appendChild(pData);
+  pGenero.innerHTML = `Genero: ${genero}.`;
+  pai.appendChild(pGenero);
 }
 
 function valida(event) {
@@ -21,8 +58,15 @@ function valida(event) {
     p.innerHTML = 'Campos inválidos';
     pai.appendChild(p);
     para(event);
+  } else {
+    limpaForm();
+    imprimeDados();
+    paraEvento(event);
   }
+
 }
+
+
 
 document.getElementById('button-login').addEventListener('click', alerta);
 document.getElementById('facebook-register').addEventListener('click', valida);
