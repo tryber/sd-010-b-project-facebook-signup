8902bc43e8d1157c2f99f96d99940d7a844ea42e
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-03-29 10:11:59 -0300

    testanto primeira implementcao do requisito 20

--- script.js
+++ script.js
@@ -47,10 +47,10 @@ function validInputRadio() {
 function paremt(valid1, valid2) {
   let value;
   if ((valid1 === true) && (valid2 === true)) {
-    value = true;
+      value = true;
   } else {
-    value = false;
-  }
+      value = false;
+    }
   return value;
 }
 
@@ -65,9 +65,41 @@ function printCampo(parametro) {
   }
 }
 
-cadastrar.addEventListener('click', () => {
+cadastrar.addEventListener('click', (event) => {
+  event.preventDefault();
   const valid1 = validFormInput();
   const valid2 = validInputRadio();
   const parametro = paremt(valid1, valid2);
-  printCampo(parametro);
+  const containerRight = document.querySelector('.right-content');
+  const childrenContainer = document.querySelector(".right-content").children
+    
+  if (parametro === true) {
+    for (let index = 0; index < childrenContainer.length; index += 1) {
+      const element = childrenContainer[index];
+      element.style.display = "none"
+    }
+
+    let userNameRegistrartion = `${document.querySelector("#name").value} ${document.querySelector("#subName").value}`;
+    let contact = document.querySelector('#phoneEmail').value;
+    let div = document.createElement('div');
+    div.className = 'right-content';
+    let paragraph = document.createElement('p')
+    paragraph.innerHTML = `Olá, ${userNameRegistrartion}
+Seu contato é ${contact}` 
+    containerRight.appendChild(paragraph);
+
+    // let birthday = document.querySelector('#date-birthdate').value;
+    // tratar data de nascimento;
+
+    } else {
+      printCampo(parametro);
+    }
+
+    const createElement = () => {
+      div.innerText = "oi"
+      console.log(div);
+    }
 });
+
+//Requisito 20
+
