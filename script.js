f05b3095071cef892b438980273f10b689389f6a
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-03-30 18:54:50 -0300

    Tentando concertar requisito 20 do projeto facebook signup

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const inputEmailPhone = document.getElementById('user-email-phone');
 const buttonGetIn = document.getElementById('button-login');
 const genderPersonality = document.getElementById('input-gender-personality');
-// const divRightContent = document.getElementsByClassName('right-content')[0];
+const divRightContent = document.getElementsByClassName('right-content')[0];
 const formOpenAccount = document.querySelector('[name="form"]');
+
+/* Requisito 20: */
+
+const getStorageInfoSignUp = () => {
+  // console.log('localizei informações do cadastro do Facebook');
+  let info = localStorage.getItem('infoUserFacebook');
+  console.log(info);
+  info = JSON.parse(info);
+  divRightContent.innerHTML = '';
+  divRightContent.innerText = `Olá, ${info.firstname} ${info.lastname}
+  ${info.phone_email}
+  ${info.birthdate}
+  ${info.gender}
+  `;
+};
+
+/* Se as informações foram preenchidas pelo usuário anteriormente, ele retorna uma mensagem na tela de login: */
+if (localStorage.getItem('infoUserFacebook')) {
+  getStorageInfoSignUp();
+}
 
 console.log(formOpenAccount);
 
@@ -29,6 +49,7 @@ const isValidFunction = (isValid, objectLocalStorage) => {
     formOpenAccount.prepend(p);
   } else {
     addLocalStorage(JSON.stringify(objectLocalStorage));
+    getStorageInfoSignUp();
   }
 };
 
@@ -68,21 +89,3 @@ genderPersonality.addEventListener('click', () => {
   input.placeholder = 'Gênero (opcional)';
   divPai.appendChild(input);
 });
-
-/* Requisito 20: */
-
-/* Se as informações foram preenchidas pelo usuário anteriormente, ele retorna uma mensagem na tela de login: */
-if (localStorage.getItem('infoUserFacebook')) {
-  let info = localStorage.getItem('infoUserFacebook');
-  console.log(info);
-  info = JSON.parse(info);
-  const className = 'firstname-lastname';
-  const firstnameLastname = document.getElementsByClassName(className)[0];
-  firstnameLastname.innerText = `Olá, ${info.firstname} ${info.lastname}`;
-  // divRightContent.innerHTML = '';
-  /* divRightContent.innerText = `Olá, ${info.firstname} ${info.lastname}
-  ${info.phone_email}
-  ${info.birthdate}
-  ${info.gender}
-  `; */
-}
