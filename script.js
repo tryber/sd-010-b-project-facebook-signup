68fb71d51402adc113b5dc7e7315fc09cb8f3dc8
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-31 20:04:47 -0300

    Requisito 19

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const clickBotao = document.getElementById('button-login');
 const clickCad = document.getElementById('facebook-register');
 const inputForm = document.querySelectorAll('main form input');
 const textForm = document.getElementById('text-form');
+const inputPersonalizado = document.getElementById('personalizado');
+
 
 function exibeAlerta() {
   alert(inputEmail.value);
@@ -24,3 +26,14 @@ clickCad.addEventListener('click', (event) => {
   }
   return inputForm;
 });
+
+inputPersonalizado.addEventListener('click', () => {
+  const newInputCreated = document.getElementById('newInput');
+  if (!newInputCreated) {
+    const newInput = document.createElement('input');
+    const divInputs = document.querySelector('.genero');
+    divInputs.appendChild(newInput);
+    newInput.placeholder = 'Gênero (opcional)';
+    newInput.id = 'newInput';
+  }
+})
