93a80327d71aad897807926a5e3d64a904350633
thosijulio <thosijulio@gmail.com>
2021-04-06 14:55:49 -0300

    resolucao exercicio 18

--- script.js
+++ script.js
@@ -6,4 +6,34 @@ function buttonLoginAlert() {
   });
 }
 
+function checkInfos() {
+  const buttonRegister = document.getElementById('facebook-register');
+  const invalidMessage = document.createElement('p');
+  invalidMessage.innerText = 'Campos inválidos';
+  buttonRegister.addEventListener('click', (evt) => {
+    const inputs = document.querySelectorAll('#facebook-form input');
+    for (let index = 0; index < 4; index += 1) {
+      if (inputs[index].value === '') {
+        evt.preventDefault();
+        buttonRegister.parentNode.appendChild(invalidMessage);
+      }
+    }
+  })
+}
+
+function checkRadios() {
+  const buttonRegister = document.getElementById('facebook-register');
+  const invalidMessage = document.createElement('p');
+  invalidMessage.innerText = 'Campos inválidos';
+  buttonRegister.addEventListener('click', (evt) => {
+    const input = document.querySelector('input[type=radio]:checked');
+    if (input === null) {
+      evt.preventDefault();
+      buttonRegister.parentNode.appendChild(invalidMessage);
+    }
+})
+}
+
 buttonLoginAlert();
+checkInfos();
+checkRadios();
