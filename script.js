fcd0171c9ca2eb0c31c5f591669bec2aa301236d
thosijulio <thosijulio@gmail.com>
2021-04-06 16:57:54 -0300

    resolucao exercicio 19

--- script.js
+++ script.js
@@ -34,6 +34,33 @@ function checkRadios() {
   });
 }
 
+function checkGenderChoised() {
+  const genderRadio = document.getElementById('gender-radio');
+  const radioChoised = document.querySelectorAll('input[type=radio]');
+  genderRadio.addEventListener('click', () => {
+    const checkGenderText = document.getElementById('customGender');
+    const form = document.getElementById('facebook-form');
+    for (let index = 0; index < radioChoised.length; index += 1) {
+      if (radioChoised[index].checked) {
+        if (radioChoised[index].id === 'Personalizado' && checkGenderText === null) {
+          createCustomGender();
+        } else if (radioChoised[index].id !== 'Personalizado' && checkGenderText) {
+          form.removeChild(checkGenderText);
+        }
+      }
+    }
+  });
+}
+function createCustomGender() {
+  const form = document.getElementById('facebook-form');
+  const buttonRegister = document.getElementById('facebook-register');
+  const input = document.createElement('input');
+  input.id = 'customGender';
+  input.placeholder = 'Gênero';
+  form.insertBefore(input, buttonRegister);
+}
+
 buttonLoginAlert();
 checkInfos();
 checkRadios();
+checkGenderChoised();
